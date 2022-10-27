f07b8383e4c2fe1abbf9ea9516bfd71515db1942
CodeFocus <codefocuschannel@gmail.com>
2022-10-26 20:35:33 -0400

    chart options fixes

--- src/components/Dashboard/Earnings/ChartContainer/ChartContainer.js
+++ src/components/Dashboard/Earnings/ChartContainer/ChartContainer.js
@@ -39,9 +39,11 @@ const createOptions = (uiCtx) => ({
     scales: {
         y: {
             grid: {
-                color: uiCtx.theme === "light" ? "#dddfe5" : "#26323f",
                 drawOnChartArea: false,
                 drawBorder: false,
+            },
+            ticks: {
+                color: uiCtx.theme === "light" ? "#929292" : "#fff",
             },
         },
         x: {
@@ -53,10 +55,10 @@ const createOptions = (uiCtx) => ({
             },
             ticks: {
                 font: {
-                    color: uiCtx.theme === "light" ? "#929292" : "#fff",
                     family: "'Mulish', sans-serif",
                     size: "16px",
                 },
+                color: uiCtx.theme === "light" ? "#929292" : "#fff",
             },
         },
     },
@@ -65,12 +67,14 @@ const createOptions = (uiCtx) => ({
 const createData = () => {
     return ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"].map(
         (month) => {
-            const profit = Math.round(Math.random() * 1000) + 200;
+            const profit = Math.round(Math.random() * 800) + 500;
+            const newUsers = ~~(Math.random() * 1000) + 50;
 
             return {
                 month,
                 sales: Math.round(profit / 5),
                 profit,
+                newUsers,
             };
         }
     );
@@ -109,6 +113,7 @@ const ChartContainer = () => {
                 borderColor: "rgba(51,200,99,.7)",
                 fill: true,
                 data: data.map(({ sales }) => sales),
+                lineTension: 0.2,
             },
             {
                 label: "Profit",
@@ -116,6 +121,15 @@ const ChartContainer = () => {
                 borderColor: "rgba(242,153,74,.7)",
                 fill: true,
                 data: data.map(({ profit }) => profit),
+                lineTension: 0.2,
+            },
+            {
+                label: "New Users",
+                backgroundColor: "rgba(166, 74, 242,.1)",
+                borderColor: "rgba(166, 74, 242,.7)",
+                fill: true,
+                data: data.map(({ newUsers }) => newUsers),
+                lineTension: 0.3,
             },
         ],
     };
